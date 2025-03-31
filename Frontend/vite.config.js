7434e1ce874a1ccca30c5c6ac8c1fd405dda3c47
Nikita-Vaid <vaidnikita06@gmail.com>
2025-03-31 16:38:30 +0530

    Nikita4

--- Frontend/vite.config.js
+++ Frontend/vite.config.js
@@ -1,7 +1,14 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  server: {
+    port: 5173,  // Explicitly set port
+    host: "0.0.0.0", // Expose server to external network
+    strictPort: true, // Prevents fallback to a different port
+  },
+});
+
+
+
